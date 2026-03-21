4215245c53863c65fec11fcb1e0ceb55b6b0ad7b
Gustav <gustavbjorelius@gmail.com>
2026-03-21 19:50:06 +0100

    first cv ship WOOOOO

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php $name = "Gustav"; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title><?php echo $name;?></title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <h1><?php echo $name; ?></h1>
+  <p>Developer. Builder. Work in progress.</p>
+  <button id="btn">Click me</button>
+  <script src="script.js"></script>
+</body>
+</html>
---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("btn").addEventListner("click", function() {
+    alert("all four languages. shipped.");
+});
