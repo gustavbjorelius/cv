2c49c4703ce5624251dcb4b1c1d2dfb6e1f24cd8
Gustav <gustavbjorelius@gmail.com>
2026-03-26 15:39:31 +0100

    notes

---
+++ inflation_adjusted_most_successful_CEOs.php
@@ -0,0 +1,3 @@
+from top to bottom, carnigie is at the top (iirc) 
+
+then do a deepdive into their unique strategies and commonalities
---
+++ most_successful_conquerors.php
@@ -0,0 +1,13 @@
+defined as
+ - took a lot of grund 
+ - 
+
+analyze 
+ - union of strategies and what they DID NOT do 
+
+examples
+ - napoleon
+ - ghengis
+ - alexander
+
+https://gemini.google.com/app/52281cc0abafbf50
