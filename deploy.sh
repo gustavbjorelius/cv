c6d175ab033fefe8d1de885ae950c8fa90fbf4f6
Gustav <gustavbjorelius@gmail.com>
2026-04-04 23:21:40 +0200

    fix: missing closing quote

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ cp -r /home/$SUDO_USER/cv/public/. $WEB_ROOT
 chown -R www-data:www-data $WEB_ROOT
 chmod -R 755 $WEB_ROOT
 systemctl reload nginx
-echo "Deployed to $WEB_ROOT
+echo "Deployed to $WEB_ROOT"
